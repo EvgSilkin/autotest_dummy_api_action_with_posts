b3205dbbc19ee73eebd2e6c87b2073931e965717
Евгений Силкин <evgsilkin9997@gmail.com>
2023-02-23 22:58:29 +0300

    devided tests to positive and negative

--- tests/test_positive_action_post.py
+++ tests/test_positive_action_post.py
@@ -26,19 +26,3 @@ def test_adding_post_by_filling_valid_data():
     Checking.check_equal_json_keys(result_post, expected_keys)
     Checking.check_json_value(result_post, "likes", 0)
     Checking.check_each_json_value(result_post, json_body)
-
-@allure.description("Adding post by filling all fields in request body with invalid data")
-def test_adding_post_by_filling_invalid_data():
-    json_body = {
-                    "text": 2023,
-                    "image": 2023,
-                    "likes": "test",
-                    "tags": 2023,
-                    "owner": 123
-                }
-    result_post: Response = Dummy_post_api.create_post(json_body)
-    Checking.assert_status_code(result_post, 400)
-    expected_keys = ["error", "data"]
-    Checking.check_equal_json_keys(result_post, expected_keys)
-    Checking.check_json_value(result_post, expected_keys[0], "BODY_NOT_VALID")
-    Checking.check_json_value(result_post, expected_keys[1], {})
